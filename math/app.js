f1a190f6b6471cf37e34c0a5540514e0af08ce92
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2020-11-22 22:05:27 -0500

    Add files via upload

---
+++ math/app.js
@@ -0,0 +1,8 @@
+
+const math = require("./math.js")
+
+console.log(math.sum(10, 20))
+
+console.log(math.subtract(30, 10))
+
+
---
+++ math/math.js
@@ -0,0 +1,14 @@
+const { multiply, factorial } = require("mathjs")
+
+function sum(a, b){
+    return a + b
+}
+function subtract(a, b){
+    return a - b
+}
+
+
+module.exports = {
+    sum: sum 
+    subtract: subtract
+    }
